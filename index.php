ffcb106608914548be06aa6dd4baedaa72c5f137
Cristina Bulnes <cristinabulnesmaya@gmail.com>
2021-02-04 15:42:39 +0100

    mainView Area display

--- index.php
+++ index.php
@@ -5,8 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Local FileSystem explorer</title>
     <link rel="stylesheet" href="Css/normalize.css">
+    <link rel="stylesheet" href="Css/mainViewArea.css">
 </head>
 <body>
-        <h1>Hello</h1>
+    <h1>Hello</h1>
+    <main class="wrapper">
+        <ul class="item-list item-list-header">
+            <li class="item-prop item-prop-header">Name</li>
+            <li class="item-prop item-prop-header">Type</li>
+            <li class="item-prop item-prop-header">Size</li>
+            <li class="item-prop item-prop-header">Created Date</li>
+            <li class="item-prop item-prop-header">Modificated</li>
+        </ul>
+    </main>
 </body>
 </html>
