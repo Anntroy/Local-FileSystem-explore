4668ccccf59cb524c7a0301fcbceb43bc95b5f32
Anntroy <ana.troyan@gmail.com>
2021-02-08 13:22:00 +0100

    Upload file functionality

---
+++ uploadFile.php
@@ -0,0 +1,50 @@
+<?php
+    function uploadFile($currentDirectory){
+
+
+        $errors = []; // Store errors here
+
+        $fileExtensionsAllowed = ['doc', 'csv', 'txt', 'ppt', 'odt', 'pdf', 'exe', 'svg', 'jpeg','jpg','png', 'mp3', 'mp4']; // These will be the only file extensions allowed
+
+        $fileName = $_FILES['the_file']['name'];
+        $fileSize = $_FILES['the_file']['size'];
+        $fileTmpName = $_FILES['the_file']['tmp_name'];
+        echo $fileTmpName;
+        $fileType = $_FILES['the_file']['type'];
+        $fileExtension = strtolower(end(explode('.',$fileName)));
+
+        $uploadPath = $currentDirectory .'/'. basename($fileName);
+        echo $currentDirectory. '<br>';
+
+        if (isset($_POST['submit'])) {
+
+            if (! in_array($fileExtension,$fileExtensionsAllowed)) {
+                $errors[] = "This file extension is not allowed.";
+            }
+            if ($fileSize > 40000000) {
+                $errors[] = "File exceeds maximum size (4MB)";
+            }
+            if (empty($errors)) {
+
+            $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+            var_dump($didUpload);
+
+
+            if ($didUpload) {
+                header("location: ./index.php");
+            } else {
+                echo "<script> alert('An error occurred. Please contact the administrator.'); </script>";
+                // header("location: ./index.php");
+            }
+        } else {
+            foreach ($errors as $error) {
+                echo "<script> alert('".$error."'); </script>";
+                // header("location: ./index.php");
+            }
+        }
+
+        }
+    };
+
+    uploadFile($_POST['currentFolder']);
+?>
