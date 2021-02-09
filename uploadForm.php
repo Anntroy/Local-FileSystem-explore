280d48dfd0363a9bd902b5a6a08f5afe23dfa12b
Anntroy <ana.troyan@gmail.com>
2021-02-09 10:49:45 +0100

    Upload file

--- uploadForm.php
+++ uploadForm.php
@@ -5,19 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Error</title>
-    <link rel="stylesheet" href="../Css/modal.css">
+    <link rel="stylesheet" href="./Css/modal.css">
 </head>
 <body>
     <?php
         echo '<article>
-                    <form class="modal__form" action="uploadFile.php" method="post" enctype="multipart/form-data">
+                    <form class="modal__form" action="./uploadFile.php" method="post" enctype="multipart/form-data">
                         <div class="modal__form-div"><h2 class="modal__form-div-h2">Upload file</h2><hr><div>
-                        <label for="file-upload" class="custom-file-upload">
+                        <label for="the_file" class="custom-file-upload">
                             <i class="fas fa-file-upload"></i>  <span>Click to choose a file</span>
                         </label>
-                            <input id="file-upload" type="file"/>
+                            <input type="file" name="the_file" id="the_file">
                             <input type="hidden" name="currentFolder" value="'.$currentFolder.'">
-                            <input type="submit" name="submit" class="btn" value="Start Upload" formaction="../uploadFile.php">
+                            <input type="submit" name="submit" class="btn" value="Start Upload">
                         </div>
                     </form>
                 </article>';
